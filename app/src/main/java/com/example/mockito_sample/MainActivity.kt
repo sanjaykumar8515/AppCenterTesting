9d610c55d00f202668b7e4c70352754a22b33d24
Sanjay <sanjaykumar8515@gmail.com>
2023-06-23 16:16:07 +0530

    Add app center dependencies

--- app/src/main/java/com/example/mockito_sample/MainActivity.kt
+++ app/src/main/java/com/example/mockito_sample/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.mockito_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCenter.start(application,"548a173b-4286-4729-932e-f25741f63304",Analytics::class.java, Crashes::class.java)
+        AppCenter.start(application,"548a173b-4286-4729-932e-f25741f63304",Analytics::class.java, Crashes::class.java  )
+
+        button = findViewById<Button>(R.id.calculateButton)
+        button.setOnClickListener {
+            Crashes.generateTestCrash()
+            //throw Exception("Something wrong")
+        }
+
     }
 }
